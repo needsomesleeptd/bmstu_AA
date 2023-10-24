e5739c4eca9d7e223d26e5d78aa8e5c2f1abf04c
Andrew Razin <razin.andrew.ru@mail.ru>
2023-10-24 13:04:32 +0300

    added time measurments + fixed parts

---
+++ prog/src/timings.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Андрей on 21/10/2023.
+//
+
+#include "timings.h"
---
+++ prog/src/timings.h
@@ -0,0 +1,16 @@
+//
+// Created by Андрей on 21/10/2023.
+//
+
+#ifndef LAB_01_AA_PROG_SRC_TIMINGS_H_
+#define LAB_01_AA_PROG_SRC_TIMINGS_H_
+
+
+
+class timings {
+
+};
+
+
+
+#endif //LAB_01_AA_PROG_SRC_TIMINGS_H_
