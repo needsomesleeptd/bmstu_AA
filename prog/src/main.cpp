1e0c229acd4908179b4dba6dd38d6b282e9a129b
Andrew Razin <razin.andrew.ru@mail.ru>
2023-11-11 02:15:14 +0300

    going to sleep

--- prog/src/main.cpp
+++ prog/src/main.cpp
@@ -3,117 +3,79 @@
 #include "sort.hpp"
 #include <locale>
 #include "timings.h"
+std::string menu = "Меню:\n"
+                   "0)Выход\n"
+                   "1)Сортировка массива с использованием сортировки перемешивания\n"
+                   "2)Сортировка массива с использованием поразрядной сортировки\n"
+                   "3)Сортировка массива с использованием блочной сортировки\n"
+                   "4)Расчет времени\n";
+
 int main()
 {
-	setlocale(LC_ALL, "");
-	/*std::wstring st1 = L"ВФА";
-	std::wstring st2 = L"АВФ";
-	Matrix mat(st1.size(),st2.size());
 
-	std::cout << "MatrixDist ==" << mat.findMatrixDistDamerau(st1, st2);
-	std::cout << std::endl;
-	mat.printMatrix();
-	std::cout << std::endl;
-	std::cout << "RecurseDistMemDamerau ==" << mat.findRecurseDistMemDamerau(st1, st2);
-	std::cout << std::endl;
-	std::cout << "RecurseDistDamerau ==" << mat.findRecurseDistDamerau(st1, st2);*/
-	//getTimeResultsWord(10, 10);
-	//std::wcin.imbue(std::locale(".OCP"));
-	//std::wcout.imbue(std::locale(".OCP"));
-	std::wstring menu = L"0.Выйти из меню\n"
-						"1.Расчет расстояния Левенштейна с помощью матрицы\n"
-						"2.Расчет расстояния Дамерау-Левенштейна с помощью матрицы\n"
-						"3.Расчет расстояния Дамерау-Левенштейна с помощью рекурсии без мемоизации\n"
-						"4.Расчет расстояния Дамерау-Левенштейна с помощью рекурсии с мемоизации\n"
-						"5.Вывести временные замеры различных методов\n"
-						"6.Сменить флаг вывода матриц\n"
-						"7.Провести расчет всеми алгоритмами";
-
-	int curCommand = 1;
-	bool matrixShowFlag = false;
-	while (curCommand != 0)
+	int request = -1;
+	while (request != 0)
 	{
-		//std::cout <<"123";
-		std::wcout << menu;
-		std::wcout<< L"\nВведите номер требуемой комманды: \n";
-		std::wcin >> curCommand;
-		if (!curCommand)
+		std::cout << menu << "\n";
+		std::cout << "Введите пункт из меню: ";
+		std::cin >> request;
+		std::cout << "\n";
+		if (request == 0)
 			break;
-		else  if (curCommand < 5 || curCommand == 7)
+		if (1 <= request && request <= 3)
 		{
-			std::wstring st1;
-			std::wstring st2;
-			std::wcout << L"Введите первое слово: \n";
-			std::wcin >> st1;
-			std::wcout << L"Введите второе слово: \n";
-			std::wcin >> st2;
-
-			Matrix mat(st1.size(), st2.size());
-			size_t distance = 0;
-			if (curCommand == 1 || curCommand == 7)
+			int n = -1;
+			while (n <= 0)
 			{
-				distance = mat.findMatrixDistLev(st1, st2);
-				if (matrixShowFlag)
+				std::cout << "Введите  размер массива для сортировки:\n";
+				std::cin >> n;
+				if (n <= 0)
+					std::cout << "Введенная размерность массива невалидна\n";
+
+
+			}
+			std::vector<int> vec(n);
+			for (int i = 0; i < n; i++)
+			{
+				std::cin >> vec[i];
+			}
+
+			switch (request)
+			{
+			case 1:
+				shakerSort(vec);
+				break;
+			case 2:
+				radixSort(vec);
+				break;
+			case 3:
+				int k = -1 ;
+				while (k <= 0)
 				{
-					std::wcout << L"\nПолученная матрица: \n";
-					mat.printMatrix();
+					std::cout << "Введите количество блоков в сортировке\n";
+					std::cin >> k;
+					if (k <= 0)
+						std::cout<< "Введенное значение количетсва блоков невалидно, попробуйте еще раз.\n";
 				}
-				std::wcout << L"\nПолученное расстояние Левенштейна с помощью матрицы: \n";
-				std::wcout << distance << L"\n";
+				blockSort(vec, k);
+				break;
 			}
-			if (curCommand == 2 || curCommand == 7)
-			{
-				distance = mat.findMatrixDistDamerau(st1, st2);
-				if (matrixShowFlag)
-				{
-					std::wcout << L"\nПолученная матрица: \n";
-					mat.printMatrix();
-				}
-				std::wcout << L"\nПолученное расстояние Дамерау-Левенштейна с помощью матрицы: \n";
-				std::wcout << distance << L"\n";
-			}
-			if (curCommand == 3 || curCommand == 7)
-			{
-				distance = mat.findRecurseDistDamerau(st1, st2);
-				std::wcout << L"\nПолученное расстояние Дамерау-Левенштейна с помощью рекурсии: \n";
-				std::wcout << distance << L"\n";
-			}
-			if (curCommand == 4 || curCommand == 7)
-			{
-				distance = mat.findRecurseDistMemDamerau(st1, st2);
-				std::wcout << L"\nПолученное расстояние Дамерау-Левенштейна с помощью рекурсии с мемоизацией: \n";
-				std::wcout << distance << L"\n";
-			}
-
-
+			std::cout << "Отсортированный массив\n";
+			for (size_t i = 0; i < vec.size(); i++)
+				std::cout << vec[i] << " ";
+			std::cout << '\n';
 		}
-		else if (curCommand == 5)
+		else
 		{
-			std::wcout << L"Введите начальную длину слова, конечную длину слова, шаг соответственно с новой строки:" << L"\n";
-			int sizeBegin = 0;
-			int sizeEnd = 0;
-			int step = 0;
-			int countIter = 0;
-			std::wcin >> sizeBegin >> sizeEnd >> step;
-			std::wcout << L"Введите количество повторений для каждой строки: ";
-			std::wcin >> countIter;
-			getTimeResults(sizeBegin,sizeEnd,step,countIter);
+			int start, stop, step;
+			int count;
+			std::cout
+				<< "Введите начальный размер массива, конечный размер и шаг изменения размера массива\n";
+			std::cin >> start >> stop >> step;
+			std::cout << "Введите количество повторений сортировки: ";
+			std::cin >> count;
+			getTimeResults(start, stop, step, count);
 		}
-		else if (curCommand == 6)
-		{
-			matrixShowFlag = !matrixShowFlag;
-			if (matrixShowFlag)
-				std::wcout << L"\nМатрицы будут выведены при следующем запуске\n";
-			else
-				std::wcout << L"\nВывод матриц отключен\n";
-		}
-
 	}
-
-	//getTimeResults(1,15,1,5);
-
-	//getMemResults(0,201,10,1);
-
-
 	return 0;
 }
--- prog/src/timings.cpp
+++ prog/src/timings.cpp
@@ -2,13 +2,103 @@
 // Created by Андрей on 10/09/2023.
 //
 #include "sort.hpp"
+
 #include <random>
 #include <iostream>
-#include <wchar.h>
 
+#include "timings.h"
 
+const double TIMEDIVISION = 1e6; //Millisecounds
+const int MAXVAL = 1e4;
 
+std::vector<int> generateVector(const size_t len)
+{
+	srand(time(NULL));
+	std::vector<int> vec(len);
 
+	std::random_device dev;
+	std::mt19937 rng(dev());
+	std::uniform_int_distribution<std::mt19937::result_type> dist(0, 1e7);
+	for (size_t i = 0; i < len; i++)
+		vec[i] = dist(rng);
 
+	return vec;
+}
 
+long getCpuTime()
+{
+	struct timespec t;
+	if (clock_gettime(CLOCK_THREAD_CPUTIME_ID, &t))
+	{
+		std::cerr << "Impossible to get time\n";
+		return -1;
+	}
+	return t.tv_sec * 1000000000LL + t.tv_nsec;
+}
+
+double getCPUTimeRadix(std::vector<int>& vec)
+{
+	long startTime;
+	long endTime;
+	//double overallTime;
+	startTime = getCpuTime();
+	radixSort(vec);
+	endTime = getCpuTime();
+	return (endTime - startTime) / TIMEDIVISION;
+}
+
+double getCPUTimeBlock(std::vector<int>& vec)
+{
+	int k = vec.size() / 2 + 2;
+	long startTime;
+	long endTime;
+	//double overallTime;
+	startTime = getCpuTime();
+	blockSort(vec, k);
+	endTime = getCpuTime();
+	return (endTime - startTime) / TIMEDIVISION;
+}
+
+double getCPUTimeShaker(std::vector<int>& vec)
+{
+	long startTime;
+	long endTime;
+	//double overallTime;
+	startTime = getCpuTime();
+	shakerSort(vec);
+	endTime = getCpuTime();
+	return (endTime - startTime) / TIMEDIVISION;
+}
+
+void getTimeResultsMatrix(size_t wordsLen, int countProcessed)
+{
+
+	std::vector<int> vec = generateVector(countProcessed);
+	double Radix = 0.0;
+	double STRASSENTime = 0.0;
+	double Block = 0.0;
+	double Shaker = 0.0;
+	for (int i = 0; i < countProcessed; ++i)
+	{
+		std::vector<int> vec_test = vec;
+		Radix += getCPUTimeRadix(vec_test);
+		Block += getCPUTimeBlock(vec_test);
+		Shaker += getCPUTimeShaker(vec_test);
+	}
+	Radix /= countProcessed;
+	STRASSENTime /= countProcessed;
+	Block /= countProcessed;
+	Shaker /= countProcessed;
+	printf("|%7zu||%15.5g||%18.5g||%29.5g|\n", wordsLen, Radix, Block, Shaker);
+	std::cout << std::flush;
+}
+
+void getTimeResults(size_t wordLenStart, size_t wordLenStop, size_t wordLenStep, int countProcessed)
+{
+	printf("\n\n|   n   ||   Поразраядная(mcs)    ||   Блочная(mcs)    ||  Перемешиванием(mcs)   |\n");
+	for (size_t i = wordLenStart; i < wordLenStop; i += wordLenStep)
+	{
+		getTimeResultsMatrix(i, countProcessed);
+	}
+}
 
