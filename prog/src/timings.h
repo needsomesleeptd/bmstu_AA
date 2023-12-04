949b4053cb4549ff46a0bef7813f77f9ec94a9d2
Andrew Razin <razin.andrew.ru@mail.ru>
2023-12-04 20:59:54 +0300

    started lab_04

--- prog/src/timings.h
+++ prog/src/timings.h
@@ -4,8 +4,6 @@
 
 #ifndef LAB_01_AA_BMSTU_AA_PROG_SRC_TIMINGS_H_
 #define LAB_01_AA_BMSTU_AA_PROG_SRC_TIMINGS_H_
-void getTimeResultsWord(size_t wordsLen,int countProcessed);
-void getTimeResults(size_t wordLenStart,size_t wordLenStop,size_t wordLenStep,int countProcessed);
-void getMemResults(size_t wordLenStart,size_t wordLenStop,size_t wordLenStep,int countProcessed);
+void recursiveMergeSort(int arr[], int left, int right);
 
 #endif //LAB_01_AA_BMSTU_AA_PROG_SRC_TIMINGS_H_
